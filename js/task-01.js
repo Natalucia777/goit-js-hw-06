59f078608fbd17d9c999d76c4553c6a6cae50e9b
Natalya Demidenko <natalucia777@ukr.net>
2023-04-06 23:26:05 +0300

    hw-1-4

--- js/task-01.js
+++ js/task-01.js
@@ -1,24 +1,15 @@
-const itemsRef = document.querySelectorAll('li.item');
+const itemsRef = document.querySelectorAll('.item');
 let indexItems = 0;
 const quantityItems = itemsRef.forEach(function (number, index) {
   indexItems += Number(index);
-  // console.log(`Number of categories: ${indexItems}`);
 });
 console.log(`Number of categories: ${indexItems}`);
+
 let titlesContent = 0;
 let titleList = 0;
-const titles = document.querySelectorAll('.item h2');
-const titleContent = titles.forEach(function (content, index) {
-  titlesContent = content.textContent;
-  titleList = document.querySelectorAll('li.item li');;
-
+itemsRef.forEach(function (content) {
+  titlesContent = content.querySelector('h2').textContent;
+  titleList = content.querySelectorAll('li');
   console.log(`Category: ${titlesContent}`);
   console.log(`Elements: ${titleList.length}`);
 });
-
-//let indexItems = 0;
-// const quantityItems = Items.forEach(function (number, index) {
-//   indexItems += Number(index);
-//   console.log(`Number of categories: ${indexItems}`);
-// });
-
