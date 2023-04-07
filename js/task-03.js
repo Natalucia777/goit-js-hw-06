815ea98121d9809eb85647b7e763bbff1c9f447e
Natalya Demidenko <natalucia777@ukr.net>
2023-04-07 11:51:22 +0300

    hw-6-3-1

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,4 @@
+const list = document.querySelector(".gallery");
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const markup = images.map(({ url, alt }) => `<li class="pet-picture"><img src="${url}" alt="${alt}"></li>`).join('');
+
+list.insertAdjacentHTML('beforeend', markup); 
+
+
