386e3783551315016d2e847475d60883e338136a
Natalya Demidenko <natalucia777@ukr.net>
2023-04-07 10:56:13 +0300

    hw-6-2-1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list1 = document.createElement("li");
+list1.textContent = "Potatoes";
+const list2 = document.createElement("li");
+list2.textContent = "Mushrooms";
+const list3 = document.createElement("li");
+list3.textContent = "Garlic";
+const list4 = document.createElement("li");
+list4.textContent = "Herbs";
+const list5 = document.createElement("li");
+list5.textContent = "Condiments";
+const listRef = document.querySelector("#ingredients");
+listRef.append(list1, list2, list3, list4, list5);
+console.log(listRef);
