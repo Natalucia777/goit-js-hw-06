9c07297b032aa2dad2e42f7ca184c1b0ff148583
Natalya Demidenko <natalucia777@ukr.net>
2023-04-08 21:14:17 +0300

    hw-6-6-1

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector("#name-input");
+let trueLength = Number(textInput.dataset.length);
+let currentLength = 0;
+textInput.addEventListener("blur", (event) => {
+  let currentLength = Number(event.currentTarget.value.length);
+  if (currentLength === trueLength) {
+    textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
+    } else {
+      textInput.classList.add("invalid");
+      textInput.classList.remove("valid");
+      }
+});
