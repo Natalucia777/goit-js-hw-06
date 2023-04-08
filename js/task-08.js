e066f1eda851e2802146644e08cb4c8f702dd8f6
Natalya Demidenko <natalucia777@ukr.net>
2023-04-08 23:43:26 +0300

    hw-6-8-1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
-
+const submitForm = document.querySelector('.login-form');
+const submitButton = document.querySelector('button');
+const emailPassword = 0;
+submitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    emailPassword = event.currentTarget = {
+    elements: { email, password },
+  };  
+  if (email.value || password.value) {
+    const emailPassword = { email: email.value, password: password.value };
+    console.log(emailPassword);
+      } else {
+      return alert("Fill in the fields please!");
+  }
+  event.currentTarget.reset();
+}); 
