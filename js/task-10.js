83c7b540951fe78ebce4162e1120ef9eb448d9b8
Natalya Demidenko <natalucia777@ukr.net>
2023-04-15 20:22:38 +0300

    hw-6-10-2

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxContent = document.querySelector('#boxes');
+const boxSize = 30;
+let size = 0;
+function createBoxes(amount) {
+   for (let i = 0; i < amount; i++) {
+     size = boxSize + i * 10;
+     
+}
+ const blockContent = `
+<div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+<div id="boxes"></div>`;
+  boxContent.insertAdjacentHTML("beforeend", blockContent);
+}
+
+function destroyBoxes() {
+
+}
+
