e99db0926c7a76d4006bcefd1f88f0149fcc311f
Natalya Demidenko <natalucia777@ukr.net>
2023-04-15 21:00:38 +0300

    hw-6-10-5

--- js/task-10.js
+++ js/task-10.js
@@ -5,13 +5,13 @@ function getRandomHexColor() {
 }
 
 const boxContent = document.querySelector('#boxes');
-const boxSize = 30;
 let size = 0;
 function createBoxes(amount) {
+  const boxSize = 30;
   for (let i = 0; i < amount; i++) {
     size = boxSize + i * 10;
     const blockContent = `<div id="boxes"></div>`;
-    blockContent.style.cssContent = `
+    blockContent.style.cssText = `
     width: ${size}px;
     height: ${size}px;
     background-color: ${getRandomHexColor()};`;
