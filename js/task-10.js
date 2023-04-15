19abf4a0214a866ae7ec1cea723cd24e87293b1d
Natalya Demidenko <natalucia777@ukr.net>
2023-04-15 20:33:11 +0300

    hw-6-10-3

--- js/task-10.js
+++ js/task-10.js
@@ -8,18 +8,15 @@ const boxContent = document.querySelector('#boxes');
 const boxSize = 30;
 let size = 0;
 function createBoxes(amount) {
-   for (let i = 0; i < amount; i++) {
-     size = boxSize + i * 10;
-     
-}
- const blockContent = `
-<div id="controls">
-  <input type="number" min="1" max="100" step="1" />
-  <button type="button" data-create>Create</button>
-  <button type="button" data-destroy>Destroy</button>
-</div>
-<div id="boxes"></div>`;
-  boxContent.insertAdjacentHTML("beforeend", blockContent);
+  for (let i = 0; i < amount; i++) {
+    size = boxSize + i * 10;
+    const blockContent = `<div id="boxes"></div>`;
+    blockContent.style.cssContent = `
+    width: ${size}px;
+    height: ${size}px;
+    background-color: ${getRandomHexColor()};`;
+    boxContent.insertAdjacentHTML("beforeend", blockContent); 
+  }
 }
 
 function destroyBoxes() {
