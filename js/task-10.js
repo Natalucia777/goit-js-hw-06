7038fe6c46019776b3f460e93879cb92675ff5b4
Natalya Demidenko <natalucia777@ukr.net>
2023-04-16 23:49:33 +0300

    hw-6-10-7

--- js/task-10.js
+++ js/task-10.js
@@ -3,25 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const boxContent = document.querySelector('#boxes');
-const controlInput = document.querySelector('#control input');
-let size = 0;
+const boxContent = document.querySelector('div#boxes');
+const controlInput = document.querySelector('input');
+const btnDataCreate = document.querySelector('[data-create]');
+btnDataCreate.addEventListener('click', () => { const val = Number(controlInput.value);
+  createBoxes(val);
+});
 function createBoxes(amount) {
-  const boxSize = 30;
-  for (let i = 0; i < amount; i++) {
-    size = boxSize + i * 10;
-    const blockContent = `<div id="boxes"></div>`;
-    blockContent.style.cssText = `
-    width: ${size}px;
-    height: ${size}px;
-    background-color: ${getRandomHexColor()};`;
-    boxContent.insertAdjacentHTML("beforeend", blockContent); 
+  const container = document.getElementById('boxes');
+  let boxSize = 30;
+  for (let i = 0; i < amount; i +=1) {
+    boxSize = boxSize + i * 10;
+    let blockContent = document.createElement('div');
+    blockContent.style.width = `${boxSize}px`;
+    blockContent.style.height = `${boxSize}px`;
+    blockContent.style.backgroundColor = getRandomHexColor();
+    boxContent.appendChild(blockContent);
   }
 }
+const btnDataDestroy = document.querySelector('[data-destroy]');
+btnDataDestroy.addEventListener('click', () => { destroyBoxes(); });
 function destroyBoxes() {
   boxContent.innerHTML = "";
 }
-const btnDataCreate = document.querySelector('[data-create]');
-btnDataCreate.addEventListener('click', () => { createBoxes(controlInput.value);});
-const btnDataDestroy = document.querySelector('[data-destroy]');
-btnDataDestroy.addEventListener('click', () => { destroyBoxes();});
